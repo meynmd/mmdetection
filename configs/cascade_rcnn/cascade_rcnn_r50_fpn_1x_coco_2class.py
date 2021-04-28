3f7ddfdf084b14c3dc531ba742a7733b14b57fe4
Matthew Meyn <matthew.meyn@stresearch.com>
2021-04-28 10:48:55 -0400

    resize to 960x544

--- configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco_2class.py
+++ configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco_2class.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/cascade_rcnn_r50_fpn_2class.py',
-    '../_base_/datasets/coco_detection.py',
+    '../_base_/datasets/coco_detection_960x544.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
