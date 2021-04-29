eb45017fe912557792c6e517c6ba096ba8fd39ba
Matthew Meyn <matthew.meyn@stresearch.com>
2021-04-29 11:31:30 -0400

    add model

--- resources/models.py
+++ resources/models.py
@@ -2,5 +2,9 @@ MODELS = {
     'cascade_rcnn': {
         'config': 'configs/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_20e_coco.py',
         'checkpoint': 'pretrained/cascade_rcnn_x101_64x4d_fpn/cascade_rcnn_x101_64x4d_fpn_20e_coco_20200509_224357-051557b1.pth'
+    },
+    'cascade_rcnn_face': {
+        'config': 'configs/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_20e_facemask.py',
+        'checkpoint': '/proj/vacsine/matt/detect/mmdetection/work_dirs/cascade_rcnn_x101_64x4d_fpn_20e_facemask/epoch_20.pth'
     }
 }
